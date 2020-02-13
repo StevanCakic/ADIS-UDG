d7ca084f0e4a8358aa7a7995afbb3a30f3402446
StevanCakic <stevancakic93@gmail.com>
2020-02-13 20:19:13 +0100

    dodat novi sadrzaj

---
+++ Nedelja 2/nedelja 2.py
@@ -0,0 +1,37 @@
+# Petlje, nastavak
+
+# Zadatak 5
+# Odraditi i sa for i sa while petljom
+
+'''
+    Napisati program koji vraća broj malih i broj velikih slova za zadati string.
+    Primjer: upper_lower(“Hi Mr. Rober. How are you today?”), vraća (19, 4), 19 - broj malih slova, 4 - broj velikih slova. Pomoć: da provjeriti da li je karakter slovo koristiti isalpha metod.
+'''
+
+# Zadatak 6
+'''
+    Napisati program koji racuna zbir parnih i proizvod neparnih brojeva od 1 do 15
+    Takodje, prikazati koliko ima parnih, a koliko neparnih brojeva iz tog segmenta
+'''
+
+# Liste
+
+# Prvi dio
+# Kreirajmo listu koja sadrži različite tipove podataka
+# Isprobajmo indeksiranje i slicing
+# Isprobajmo spajanje više listi u jednu
+
+# Drugi dio
+# Metode za liste
+
+# Kreiranje matrice
+
+# Razlika između append i extend
+
+# Torke
+
+# Funkcije
+
+# Dictionaries
+
+# Skupovi
