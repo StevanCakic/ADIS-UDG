8c3ff045c998bf22e2a91c5aa6cf8d3a9b917b5d
StevanCakic <stevancakic93@gmail.com>
2020-03-02 09:59:00 +0100

    new code attached

--- Nedelja 5/files.py
+++ Nedelja 5/files.py
@@ -1,8 +1,52 @@
 # Files
+'''
+f = open("test.txt")
+print(f.read())
+print(f.read())
 
+f.seek(0)
+print(f.read())
+print("***********")
+f.seek(0)
+for line in f:
+    print(line)
+f.close()
+
+f = open("test.txt", mode="a")
+f.write("\nNovi tekst iz Pythona\n")
+f.close()
+
+with open("test.txt") as file:
+    print(file.read())
+
+import os
+
+
+if os.path.exists("./fajlovi/test.txt"):
+    os.remove("./fajlovi/test.txt")
+
+for file in os.listdir("./fajlovi"):
+    os.remove("./fajlovi/" + file)
+os.rmdir("fajlovi")
+'''
 # Zadatak 1
-# Iz fajlova brojevi.txt izdvojiti sve dvocifrene i trocifrene brojeve
+# Iz fajla brojevi.txt izdvojiti sve dvocifrene i trocifrene brojeve
 # i upisati ih u novi fajl odabrani_brojevi.txt
+
+'''
+dvocifreni_trocifreni = []
+with open("brojevi.txt") as brojevi:
+    lines = brojevi.read().split("\n")
+    print(lines)
+    for line in lines:
+        if 1 < len(line) < 4:
+            dvocifreni_trocifreni.append(line)
+print(dvocifreni_trocifreni) 
+
+with open("odabrani_brojevi", mode="w") as new_file:
+    for broj in dvocifreni_trocifreni:
+        new_file.write(broj + "\n")
+'''
 
 # Zadatak 2
 # U fajlu studenti nalazi se lista studenata sa ocjenama za zadati predmet.
@@ -10,6 +54,33 @@
 # Napisati funkciju koja vraca prosjecnu ocjenu ostvarenu na ispitu.
 # E [6 - 6.5), D [6.5, 7.5), C [7.5, 8.5), B [8.5, 9.5), A [9.5, 10]
 # Studente koji su dobili ocjenu F ne ukljucivati u prosjek
+suma = 0
+broj_studenata = 0
+prosjek = 1
+with open("studenti.txt") as fajl:
+    lines = fajl.read().split("\n")
+    
+    for line in lines:
+        student_info = line.split()
+        print(student_info)
+        if student_info[2] == "A":
+            suma += 10
+            broj_studenata +=1
+        elif student_info[2] == "B":
+            suma += 9
+            broj_studenata +=1
+        elif student_info[2] == "C":
+            suma += 8
+            broj_studenata +=1
+        elif student_info[2] == "D":
+            suma += 7
+            broj_studenata +=1
+        elif student_info[2] == "E":
+            suma += 6
+            broj_studenata +=1
+    prosjek = suma / broj_studenata
+
+print(prosjek)
 
 # Zadatak 3
 
